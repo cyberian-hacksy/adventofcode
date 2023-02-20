fbfb755ecbf103c6892f699012397b22837e7728
Cyberian Hacksy <cyberian.hacksy@gmail.com>
2023-02-20 18:15:09 -0500

    ChatGPT powered refactoring to avoid repetitiveness

--- src/main/kotlin/adventofcode2022/day1.kt
+++ src/main/kotlin/adventofcode2022/day1.kt
@@ -3,35 +3,11 @@ package adventofcode2022
 fun main() {
     val data = ClassLoader.getSystemResource("adventofcode2022/day1.txt").readText().lines()
         .map { s -> if (!s.isEmpty()) s.toInt() else 0 }
-    day1Solution1(data)
-    day1Solution2(data)
+    day1Solution(data, 1)
+    day1Solution(data, 3)
 }
 
-fun day1Solution1(calories: List<Int>) {
-    var maxCalories = 0
-    var currentElf = 0
-    var currentCalories = 0
-
-    for (i in calories.indices) {
-        if (calories[i] > 0) {
-            currentCalories += calories[i]
-        } else {
-            if (currentCalories > maxCalories) {
-                maxCalories = currentCalories
-            }
-            currentElf++
-            currentCalories = 0
-        }
-    }
-
-    if (currentCalories > maxCalories) {
-        maxCalories = currentCalories
-    }
-
-    println(maxCalories)
-}
-
-fun day1Solution2(calories: List<Int>) {
+fun day1Solution(calories: List<Int>, numElves: Int) {
     val elfCalories = mutableMapOf<Int, Int>()
     var elfNumber = 0
 
@@ -40,6 +16,7 @@ fun day1Solution2(calories: List<Int>) {
         if (calorie == 0) elfNumber++
     }
 
-    println(elfCalories.toList().sortedByDescending { (_, value) -> value }.take(3).map { (_, value) -> value }.sum())
+    val maxCalories =
+        elfCalories.toList().sortedByDescending { (_, value) -> value }.take(numElves).map { (_, value) -> value }.sum()
+    println(maxCalories)
 }
-
--- src/main/kotlin/adventofcode2022/day4.kt
+++ src/main/kotlin/adventofcode2022/day4.kt
@@ -2,35 +2,27 @@ package adventofcode2022
 
 fun main() {
     val data = ClassLoader.getSystemResource("adventofcode2022/day4.txt").readText().lines()
-    day4Solution1(data)
-    day4Solution2(data)
+    day4Solution(data, 1)
+    day4Solution(data, 2)
 }
 
-fun day4Solution1(sections: List<String>) {
+fun day4Solution(sections: List<String>, solutionType: Int) {
     var count = 0
     for (assignment in sections) {
         val ranges = assignment.split(",")
         val range1 = ranges[0].split("-").map { it.toInt() }
         val range2 = ranges[1].split("-").map { it.toInt() }
-        if ((range1[0] >= range2[0]) && (range1[1] <= range2[1])) {
-            count++
-        } else if ((range1[0] <= range2[0]) && (range1[1] >= range2[1])) {
-            count++
+        if (solutionType == 1) {
+            if ((range1[0] >= range2[0]) && (range1[1] <= range2[1])) {
+                count++
+            } else if ((range1[0] <= range2[0]) && (range1[1] >= range2[1])) {
+                count++
+            }
+        } else if (solutionType == 2) {
+            if ((range1[0]..range1[1]).intersect(range2[0]..range2[1]).isNotEmpty()) {
+                count++
+            }
         }
     }
     println(count)
 }
-
-fun day4Solution2(sections: List<String>) {
-    var count = 0
-    for (assignment in sections) {
-        val ranges = assignment.split(",")
-        val range1 = ranges[0].split("-").map { it.toInt() }
-        val range2 = ranges[1].split("-").map { it.toInt() }
-        if ((range1[0]..range1[1]).intersect(range2[0]..range2[1]).isNotEmpty()) {
-            count++
-        }
-    }
-    println(count)
-}
-
